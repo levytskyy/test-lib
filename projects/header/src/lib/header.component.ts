8d11564b9c912659fa8f883253f2402a657e7cf7
yuriy <yuriy.levytskyy@gmail.com>
2025-03-14 17:12:48 +0100

    add files

---
+++ projects/header/src/lib/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-header',
+  imports: [],
+  template: `
+    <p>
+      header works!
+    </p>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
